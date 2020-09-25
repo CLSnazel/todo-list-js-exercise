78550758f0886e2b5036fef860a087cb6981e2ee
Chantal Snazel <clsnazel@gmail.com>
2020-09-25 19:40:14 +0000

    completed OOP activity

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+function newTask(title, desc) {
+  // taskTitles.push(title);
+  // taskComplete.push(false);
+  // taskDescriptions.push(desc);
+  const task = {
+    title: title,
+    description: desc,
+    complete: false,
+    markCompleted: function() {
+      this.complete = true;
+    },
+    logTaskState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    }
+  }
+  return task
 }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
-
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+const task1 = newTask("Clean Cat Litter", "Go to the laundry room and clean the litter."); // task 0
+const task2 = newTask("Do Laundry", "Make sure to separate into like colors and wash on cold."); // task 1
+const tasks = [task1, task2];
+// console.log(tasks);
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logTaskState(); // Clean Cat Litter has been completed
